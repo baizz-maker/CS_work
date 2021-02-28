d4dfbe60f25a176f3803f17ab3e344dd0a47a6cf
baizz-maker <55344134+baizz-maker@users.noreply.github.com>
2021-02-28 22:23:20 +0800

    Add files via upload

---
+++ homework1/Calculator_console/Calculator_console/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Calculator_console
+{
+    class Program
+    {   
+
+        static void Main(string[] args)
+        {
+            string text;
+            int num1;
+            int num2;
+            char calculate;
+            Console.WriteLine("Input the first number:");
+            text = Console.ReadLine();
+            num1 = int.Parse(text);
+            Console.WriteLine("Input the second number:");
+            text = Console.ReadLine();
+            num2 = int.Parse(text);
+            Console.WriteLine("Input the operator:");
+            text = Console.ReadLine();
+            switch (text)
+            {
+                case "+":
+                    Console.WriteLine("The result is:  " + ( num1 + num2));
+                    break;
+                case "-":
+                    Console.WriteLine("The result is:  " + (num1 - num2));
+                    break;
+                case "*":
+                    Console.WriteLine("The result is:  " + (num1 * num2));
+                    break;
+                case "/":
+                    Console.WriteLine("The result is:  " + (num1 / num2));
+                    break;
+
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ homework1/Calculator_windowsform/Calculator_windowsform/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_windowsform
+{
+    public partial class Form1 : Form
+    {
+        double num1;
+        double num2;
+        double result_num;
+        string oper;
+        string result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            num1 = double.Parse(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            num2 = double.Parse(textBox2.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (oper)
+            {
+                case "+":
+                    result_num = num1 + num2;
+                    break;
+                case "-":
+                    result_num = num1 - num2;
+                    break;
+                case "*":
+                    result_num = num1 * num2;
+                    break;
+                case "/":
+                    result_num = num1 / num2;
+                    break;
+
+            }
+            result = result_num.ToString();
+            textBox3.Text = result;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            oper = comboBox1.Text;
+        }
+    }
+}
