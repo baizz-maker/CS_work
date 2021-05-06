cc624311dfa2accfdbb6f99b61bce71412427ea9
baizz-maker <55344134+baizz-maker@users.noreply.github.com>
2021-05-06 23:00:51 +0800

    Add files via upload

---
+++ homework10/Crawler.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace homework9
+{
+    class Crawler
+    {
+        public event Action<string> InfoTextChange;
+        public static readonly string urlParseRegex = @"^(?<site>(?<protocal>https?)://(?<host>[\w.-]+)(:\d+)?($|/))(\w+/)*(?<file>[^#?]*)";
+        public Hashtable urls = new Hashtable();
+        private Queue<StringBuilder> parselist = new Queue<StringBuilder>();
+        private int count = 0;
+        private int queueamount = 0;
+        private string current = null;
+        public int depth { get; set; } = 10;
+
+
+        /*public void Crawl()
+        {
+            while (true)
+            {
+                try { 
+                    foreach (string url in urls.Keys)
+                    {
+                        if ((bool)urls[url]) continue;
+                        current = url;
+                    }
+
+                    if (current == null || count > depth) break;
+                    string html = DownLoad(current); // 下载
+                    urls[current] = true;
+                    string info = current + "  SUCCESS"+"\r\n";
+                    InfoTextChange(info);
+                    count++;
+                    Parse(html, current);//解析,并加入新的链接
+                    }
+                catch(Exception e)
+                {
+                    string info = current + "  ERROR" + "\r\n";
+                    InfoTextChange(info);
+                }
+            }
+        }*/
+
+        public void CreateDownloadList()
+        {
+            while (true)
+            {
+                if (count > depth) break;
+
+                if (queueamount > 0)
+                {
+                    string url_html_num = parselist.Dequeue().ToString();
+                    queueamount--;
+                    int url_length = int.Parse(url_html_num.Substring(url_html_num.Length - 1, 1));
+
+                    Parse(url_html_num.Substring(url_length, url_html_num.Length - 1 - url_length), url_html_num.Substring(0, url_length));
+
+                }
+            }
+        }
+
+        public void Crawl()
+        {
+            while (true)
+            {
+                try
+                {
+                    foreach (string url in urls.Keys)
+                    {
+                        if ((bool)urls[url]) continue;
+                        current = url;
+                    }
+
+                    if (current == null || count > depth) break;
+                    string html = DownLoad(current); // 下载
+                    urls[current] = true;
+                    StringBuilder url_html = new StringBuilder(current).Append(new StringBuilder(html)).Append(new StringBuilder(current.Length));
+                    parselist.Enqueue(url_html);
+                    string info = current + "  SUCCESS" + "\r\n";
+                    InfoTextChange(info);
+                    count++;
+                    queueamount++;
+                }
+                catch (Exception e)
+                {
+                    string info = current + "  ERROR" + "\r\n";
+                    InfoTextChange(info);
+                }
+            }
+        }
+
+        public string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+
+        private void Parse(string html, string currenturl)
+        {
+            string strRef = @"(href|HREF)[]*=[]*[""'][^""'#>]+[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '\"', '#', '>');
+                if (strRef.Length == 0) continue;
+                strRef = FixUrl(strRef, currenturl);
+                if (urls[strRef] == null) urls[strRef] = false;
+            }
+        }
+
+        static private string FixUrl(string url, string pageUrl)
+        {
+            if (url.Contains("://"))
+            { 
+                return url;
+            }
+            if (url.StartsWith("//"))
+            {
+                Match urlMatch = Regex.Match(pageUrl, urlParseRegex);
+                string protocal = urlMatch.Groups["protocal"].Value;
+                return protocal + ":" + url;
+            }
+            if (url.StartsWith("/"))
+            {
+                Match urlMatch = Regex.Match(pageUrl, urlParseRegex);
+                String site = urlMatch.Groups["site"].Value;
+                return site.EndsWith("/") ? site + url.Substring(1) : site + url;
+            }
+
+            if (url.StartsWith("../"))
+            {
+                url = url.Substring(3);
+                int idx = pageUrl.LastIndexOf('/');
+                return FixUrl(url, pageUrl.Substring(0, idx));
+            }
+
+            if (url.StartsWith("./"))
+            {
+                return FixUrl(url.Substring(2), pageUrl);
+            }
+            
+            int end = pageUrl.LastIndexOf("/");
+            return pageUrl.Substring(0, end) + "/" + url;
+        }
+    }
+}
---
+++ homework10/baseForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework9
+{
+    public partial class baseForm : Form
+    {
+
+
+        Crawler myCrawler = new Crawler();
+        string startUrl = "https://www.baidu.com";
+
+        public baseForm()
+        {
+            InitializeComponent();
+            myCrawler.InfoTextChange += PrintInfo;
+        }
+
+        private void PrintInfo(string info)
+        {
+            Action action = () => info_textBox.AppendText(info);
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+
+            myCrawler.urls.Add(startUrl, false);//加入初始页面
+            //new Thread(myCrawler.Crawl).Start();
+            Parallel.Invoke(new Action[] { 
+                () => myCrawler.CreateDownloadList(),
+                () => myCrawler.Crawl()
+ 
+            });
+            /*result_FileDialog.InitialDirectory = @"C:\Users\86187\source\repos\homework9\homework9\bin\Debug";
+            result_FileDialog.ShowDialog();*/
+        }
+
+        private void target_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+            startUrl = target_textBox.Text;
+        }
+
+        private void depth_textBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                myCrawler.depth = int.Parse(depth_textBox.Text);
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show("DEPTH INPUT ERROR");
+            }
+        }
+    }
+}
